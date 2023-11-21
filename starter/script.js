646d6ebdb0b13344331eb180dd67bc19667fa117
-Xara- Siddiqui. <zara.slough@gmail.com>
2023-11-21 19:27:36 +0000

    Added function to prompt user for password options

--- starter/script.js
+++ starter/script.js
@@ -90,6 +90,34 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to be? Enter a number between 8 and 128')
+  );
+
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert('Password length must be a number between 8 and 128 characters');
+    return null;
+  }
+
+  var hasSpecialCharacters = confirm('Click OK to include special characters.');
+  var hasNumericCharacters = confirm('Click OK to include numeric characters.');
+  var hasLowerCasedCharacters = confirm('Click OK to include lowercase characters.');
+  var hasUpperCasedCharacters = confirm('Click OK to include uppercase characters.');
+
+  if (!hasSpecialCharacters && !hasNumericCharacters && !hasLowerCasedCharacters && !hasUpperCasedCharacters) {
+    alert('Must select at least one character type');
+    return null;
+  }
+
+  var passwordOptions = {
+    length: length,
+    hasSpecialCharacters: hasSpecialCharacters,
+    hasNumericCharacters: hasNumericCharacters,
+    hasLowerCasedCharacters: hasLowerCasedCharacters,
+    hasUpperCasedCharacters: hasUpperCasedCharacters
+  };
+
+  return passwordOptions;
 
 }
 
